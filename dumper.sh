aab33e69f701766bd23af11f07620b9d7e1edae4
Leonid <leonidkoida@gmail.com>
2016-07-16 18:28:03 +0300

    Small reactor
    removed "sudo"

--- dumper.sh
+++ dumper.sh
@@ -5,7 +5,7 @@ backup_path="/var/www/dumps"
 #need to set actual name for database 
 #add this script to crontab twice in a week
 #save in 2 places(local and maybe remote and send weekly report(in future))
-sudo mysqldump designer_db > db_$date.sql
+mysqldump designer_db > db_$date.sql
 
 if [ -d $backup_path/dump_$date ]
 	then
